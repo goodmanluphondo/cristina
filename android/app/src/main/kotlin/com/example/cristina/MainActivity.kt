08dca737a77561cedf62e9021e9f58f8f7e36d3f
Goodman Luphondo <luphondog@outlook.com>
2020-11-14 18:44:04 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/cristina/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cristina
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Cristina',
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Cristina'),
+          ),
+          body: Center(
+            child: RandomWords(),
+          )),
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
+  }
+}
